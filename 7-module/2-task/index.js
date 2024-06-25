9c981efa5de04b64ea55265174b176fb15f322b3
Simon Sklyarov <simvaliant@gmail.com>
2024-06-25 21:18:39 +0300

    difficult-components

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,84 @@
 import createElement from '../../assets/lib/create-element.js';
 
-export default class Modal {
+export default class Modal { 
   constructor() {
+    this.elem = this.render();
+  }
+
+  template = () => {
+    return `
+    <div class="modal">
+    
+      <div class="modal__overlay"></div>
+
+      <div class="modal__inner">
+        <div class="modal__header">
+          
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+
+          <h3 class="modal__title">
+            
+          </h3>
+        </div>
+
+        <div class="modal__body">
+          
+        </div>
+      </div>
+
+    </div>
+    `;
+  }
+
+  open = () => {
+    // const container = document.querySelector('.container');
+    document.body.append(this.elem);
+
+    document.body.classList.add('is-modal-open');
+  }
+
+  close = () => {
+    this.elem.remove();
+
+    document.body.classList.remove('is-modal-open');
+  }
+
+  
+  btnClose = () => {
+    const btn = this.elem.querySelector('.modal__close');
+    btn.addEventListener('click', () => {
+      this.close();
+    })
+  }
+  
+
+  keyClose = () => {
+    document.addEventListener('keydown', event => {
+      if(event.code == 'Escape'){
+        this.close();
+      }
+    }, {once: true});
+  }
+
+  setTitle = (title) => {
+    const modalTitle = this.elem.querySelector('.modal__title');
+    modalTitle.textContent = title;
+  }
+
+  setBody = (html) => {
+    const modalBody = this.elem.querySelector('.modal__body');
+    modalBody.innerHTML = '';
+    modalBody.append(html);
+  }
+
+  render = () => {
+
+    this.elem = createElement(this.template());
+    this.btnClose();
+    this.keyClose();
+    
+    return this.elem;
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,73 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.elem = this.render();
+  }
+
+  template = () => {
+
+    let result = '<span class="slider__step-active"></span>';
+
+    for(let i = 1; i < this.steps; i++){
+      result+=`<span></span>`;
+    }
+
+    return `
+    <div class="slider">
+
+      <div class="slider__thumb" style="left: 0%;">
+        <span class="slider__value">0</span>
+      </div>
+
+      <div class="slider__progress" style="width: 0%;"></div>
+
+      <div class="slider__steps">
+    ` + result + `
+      </div>
+    </div>
+    `
+  }
+
+  onClick = () => {
+    this.elem.addEventListener('click', (event) => {
+      let thumb = this.elem.querySelector('.slider__thumb');
+      let progress = this.elem.querySelector('.slider__progress');
+      let sliderValue = this.elem.querySelector('.slider__value');
+      let sliderSteps = this.elem.querySelector('.slider__steps')
+      let steps = sliderSteps.querySelectorAll('span');
+
+      let left = event.clientX - this.elem.getBoundingClientRect().left;
+      let leftRelative = left / this.elem.offsetWidth;
+      let segments = this.steps - 1;
+      let approximateValue = leftRelative * segments;
+      let value = Math.round(approximateValue);
+
+      sliderValue.textContent = value;
+      for(let step of steps){
+        step.classList.remove('slider__step-active');
+      }
+      steps[value].classList.add('slider__step-active');
+
+      let changeStep = new CustomEvent('slider-change', {
+        detail: value,
+        bubbles: true
+      });
+
+      this.elem.dispatchEvent(changeStep);
+      
+      let valuePercents = value / segments * 100;
+      thumb.style.left = `${valuePercents}%`;
+      progress.style.width = `${valuePercents}%`;
+    })
+  }
+
+  render = () => {
+     this.elem = createElement(this.template());
+     this.onClick();
+
+     return this.elem;
   }
 }
